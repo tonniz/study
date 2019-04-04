af902eb135c20f44228b14099f81c67ac6a4100d
zhangxingxin <zhang_xx@murongtech.com>
2019-04-04 11:11:06 +0800

    增加chapter1工程

---
+++ chapter1/src/test/java/com/ronglian/study/springboot/chapter1/controller/HelloControllerTest.java
@@ -0,0 +1,39 @@
+package com.ronglian.study.springboot.chapter1.controller;
+
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.context.web.WebAppConfiguration;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import org.springframework.test.web.servlet.result.MockMvcResultHandlers;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+import org.springframework.web.context.WebApplicationContext;
+import static org.hamcrest.Matchers.equalTo;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+@RunWith(SpringRunner.class)
+@SpringBootTest
+@WebAppConfiguration
+public class HelloControllerTest {
+    MockMvc mockMVC;
+    @Autowired
+    WebApplicationContext context;
+
+    @Before
+    public void initMockMvc() {
+        mockMVC = MockMvcBuilders.webAppContextSetup(context).build();
+    }
+
+    @Test
+    public void index() throws Exception {
+        mockMVC.perform(MockMvcRequestBuilders.get("/hello").accept(MediaType.APPLICATION_JSON))
+                .andExpect(status().isOk())
+                .andExpect(content().string(equalTo("Hello World")))
+                .andDo(MockMvcResultHandlers.print());
+    }
+}
